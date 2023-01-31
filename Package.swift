90584c0849ce6db9f7d2b5f4a6ae2bfcda46d074
sandabu <nao0101.n@gmail.com>
2023-01-31 09:50:57 +0900

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SkyWaySFUBot",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SkyWaySFUBot",
+            targets: ["SkyWayCore", "SkyWaySFUBot", "WebRTC"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "SkyWayCore",
+            url: "https://github.com/skyway/ios-sdk/releases/download/1.0.0/SkywayCore.xcframework.zip",
+            checksum: "e86d13e1099a6a48b33dccc3e93931e036d982479dd5fbc6af5c94137a4d1b0f"),
+        .binaryTarget(
+            name: "SkyWaySFUBot",
+            url: "https://github.com/skyway/ios-sdk/releases/download/1.0.0/SkyWaySFUBot.xcframework.zip",
+            checksum: "72b1fec6ac742c571ad36575b5b54009b25e3db1d8c911b6a50e3b2a509f8607"),
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://github.com/skyway/skyway-ios-webrtc-specs/releases/download/104.0.0/WebRTC.xcframework.zip",
+            checksum: "84722bec35c919945164edfa3f0577fc1c29bf2a14033d635bbe0b86ed0ef088"),
+
+    ],
+    swiftLanguageVersions: [.v5]
+)
---
+++ README.md
@@ -0,0 +1,7 @@
+# SkyWay iOS SFU Bot SDK for Swift Package 
+
+SFU Bot ライブラリのSwift Package Manager管理リポジトリ
+
+iOS SDK リポジトリ
+
+https://github.com/skyway/ios-sdk
